74a97eb1f087e4552bccd067e89164e29ce64e63
xiaohe <1546817560@qq.com>
2019-12-14 15:47:28 +0800

    去除sql 中的编码字符集

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -6,6 +6,6 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `test`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '名称',
+  `name` varchar(255) NULL DEFAULT NULL COMMENT '名称',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB ROW_FORMAT = Dynamic;
